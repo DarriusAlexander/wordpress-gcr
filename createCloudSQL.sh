dbbc53efdac1cd217fc7935272d665e64820b413
Darrius Alexander <darriush@voltlighting.com>
2020-08-05 16:55:08 -0400

    ..
    ..

--- createCloudSQL.sh
+++ createCloudSQL.sh
@@ -2,9 +2,9 @@
 
 echo "This script automates the creation of a CloudSQL Instance & database on GCP"
 
-read -p "Enter Chosen Name for the CloudSQL Instance: "  instanceName
-read -p "Enter Chosen Database Name: "  databaseName
-read -p "Enter root password for the CloudSQL Instance: "  passWord
+read -p "Enter Chosen Name for the CloudSQL Instance: "  wp
+read -p "Enter Chosen Database Name: "  wordpress
+read -p "Enter root password for the CloudSQL Instance: "  wordpress
 
 
 gcloud sql instances create $instanceName --activation-policy=ALWAYS --tier=db-n1-standard-1
